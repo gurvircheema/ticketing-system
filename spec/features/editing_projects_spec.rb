2ad8a2392884ddcd3bc62b80f25a31d3ab652e79
Gurvir Cheema <gurvircheema@hotmail.com>
2017-12-14 12:59:04 -0800

    Projects can now be updated

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can edit existing projects' do
+  before do
+    FactoryBot.create(:project, name: "Sublime Text 3")
+    visit "/"
+    click_link "Sublime Text 3"
+    click_link "Edit Project"
+  end
+  scenario 'with valid attributes' do
+    fill_in 'Name', with: 'Code'
+    click_button 'Update Project'
+    expect(page).to have_content 'Project has been updated'
+    expect(page).to have_content 'Code'
+  end
+  scenario 'when providing invalid attributes' do
+    fill_in 'Name', with: ''
+    click_button 'Update Project'
+    expect(page).to have_content 'Project has not been updated'
+  end
+end
