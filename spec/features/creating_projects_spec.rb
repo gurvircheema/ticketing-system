58f9d3b2c606293c5c1ed8e21244ef026c73eaa6
Gurvir Cheema <gurvircheema@hotmail.com>
2017-12-13 23:02:34 -0800

    Add validations to project model

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,19 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can create new projects' do
-  scenario 'with valid attributes' do
+  before do
     visit '/'
-    click_link 'New Project' do
-      fill_in 'Name', with: 'Sample Project'
-      fill_in 'Description', with: 'A Random sentence for describing a sample'
-      click_button 'Create Project'
-      expect(page).to have_content 'Project has been created.'
+    click_link 'New Project'
+  end
 
-      project = Project.find_by(name: 'Sample Project')
-      expect(page.current_url).to eq project_url(project)
+  scenario 'with valid attributes' do
+    fill_in 'Name', with: 'Sample Project'
+    fill_in 'Description', with: 'A Random sentence for describing a sample'
+    click_button 'Create Project'
+    expect(page).to have_content 'Project has been created.'
 
-      title = 'Sample Project - Projects - Ticketee'
-      expect(page).to have_title title
-    end
+    project = Project.find_by(name: 'Sample Project')
+    expect(page.current_url).to eq project_url(project)
+
+    title = 'Sample Project - Projects - Ticketee'
+    expect(page).to have_title title
+  end
+
+  scenario 'with invalid attributes' do
+    visit '/'
+    click_link 'New Project'
+    click_button 'Create Project'
+    expect(page).to have_content 'Project has not been created'
+    expect(page).to have_content "Name can't be blank"
   end
 end
