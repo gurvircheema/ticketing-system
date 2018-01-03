ab488e6e3210356c24e33679e15be6e77725b790
Gurvir Cheema <gurvircheema@hotmail.com>
2018-01-03 01:03:24 -0800

    Archived users can not sign in

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -7,9 +7,19 @@ RSpec.feature 'Users can sign in' do
     visit '/'
     click_link 'Sign in'
     fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password 
+    fill_in 'Password', with: user.password
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully'
     expect(page).to have_content "Signed in as #{user.email}"
   end
-end 
+
+  scenario 'unless they are archived' do
+    user.archive
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_content 'Your account has been archived.'
+  end
+end
