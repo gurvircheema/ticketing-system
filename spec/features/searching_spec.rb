a4bfa923e5958b23cf833f6f13d0ec8ab3d6cf43
Gurvir Cheema <gurvircheema@hotmail.com>
2018-01-05 15:08:25 -0800

    Users can search by state or tag

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.feature 'User can search for ticket matching specific criteria' do
   let(:user) { FactoryBot.create :user }
   let(:project) { FactoryBot.create :project }
+  let(:open) { State.create(name: 'Open', default: true) }
+  let(:closed) { State.create(name: 'Closed') }
+
   let(:ticket_1) do
     FactoryBot.create(
       :ticket, name: 'First one', project: project,
-      author: user, tag_names: 'iteration1')
+      author: user, tag_names: 'iteration1', state: open)
   end
   let(:ticket_2) do
     FactoryBot.create(
       :ticket, name: 'Create users', project: project,
-      author: user, tag_names: 'iteration2')
+      author: user, tag_names: 'iteration2', state: closed)
   end
 
   before do
@@ -25,6 +28,15 @@ RSpec.feature 'User can search for ticket matching specific criteria' do
     click_button 'Search'
     within('#tickets') do
       # expect(page).to have_link 'First one'
+      expect(page).not_to have_link 'Create users'
+    end
+  end
+
+  scenario 'searching by state' do
+    fill_in 'Search', with: 'state:Open'
+    click_button 'Search'
+    within('#tickets') do
+      # expect(page).to have_link 'Create projects'
       expect(page).not_to have_link 'Create users'
     end
   end
